f6a7127339e2ca73ff05792e46dd65b00c8e57a8
Roudra <103403881+roudramondal@users.noreply.github.com>
2024-11-10 15:22:42 +0600

    Add files via upload
    input and output

---
+++ in_and_out.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main ()
+{
+  int a, b;
+  printf("enter number  a and b=  ");
+  scanf("%d %d", &a, &b);
+  printf("The value of a is = %d", a);
+  printf("\n");
+  printf("The value of b is = %d", b);
+  return 0;
+}
