9047dcaf57a2f3527deb50d5fb91f628fb1b268c
Roudra <103403881+roudramondal@users.noreply.github.com>
2024-11-11 22:25:17 +0600

    Add files via upload

---
+++ problem12.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main ()
+{
+  int x, y;
+  scanf("%d %d", &x, &y);
+  if(x<=100)
+  {
+    if (y<=10)
+    {
+      if ((int)x+y>=100)
+      {
+        printf("YES\n");
+      }
+      else 
+      {
+        printf("NO\n");
+      }
+    }
+    else
+    {
+
+    }
+    
+  }
+  else 
+    {
+      
+    }
+
+  return 0;
+}
