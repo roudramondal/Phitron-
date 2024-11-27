8394883d07b37fad27fd8ab30b7622d7dc43e8c7
Roudra <103403881+roudramondal@users.noreply.github.com>
2024-11-27 23:41:58 +0600

    Add files via upload

--- problem1.c
+++ problem1.c
@@ -3,9 +3,14 @@ int main ()
 {
   int n;
   scanf("%d", &n);
-  for (int i = 1; i <= n;i++)
+  int a[n];
+  for (int i = 0; i < n;i++)
   {
-    printf("%d ", i);
+    scanf("%d", &a[i]);
+  }
+  for (int i = n - 1; i >= 0;i--)
+  {
+    printf("%d ", a[i]);
   }
   return 0;
 }
